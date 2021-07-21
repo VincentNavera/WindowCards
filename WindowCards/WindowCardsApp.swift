05fde0231190d89906c7cce85b340ce51b73101f
v1ncio <airfoil_elector0b@icloud.com>
2021-07-21 19:16:42 +0800

    Initial Commit

---
+++ WindowCards/WindowCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WindowCardsApp.swift
+//  WindowCards
+//
+//  Created by Vincio on 7/21/21.
+//
+
+import SwiftUI
+
+@main
+struct WindowCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
